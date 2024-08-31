cfd23da26ec8325a5860aebea98abe1fc6349358
leomelegari <leonardo@leonardos-iMac-Pro.local>
2024-08-31 11:10:26 -0300

    created hook to confirm when something want to be removed

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -17,6 +17,8 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { useConfirm } from "@/hooks/use-confirm";
 
 interface PreferencesModalProps {
   open: boolean;
@@ -29,7 +31,12 @@ export default function PreferencesModal({
   open,
   setOpen,
 }: PreferencesModalProps) {
+  const router = useRouter();
   const workspaceId = useWorkspaceId();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "This action is irreversible!",
+  );
 
   const [value, setValue] = useState<string>(initialValue);
   const [editOpen, setEditOpen] = useState<boolean>(false);
@@ -60,15 +67,19 @@ export default function PreferencesModal({
     );
   };
 
-  const handleRemove = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleRemove = async () => {
+    const ok = await confirm();
+
+    if (!ok) {
+      return;
+    }
 
     removeWorkspace(
       { id: workspaceId },
       {
         onSuccess() {
+          router.replace("/");
           toast.success("Workspace removed!");
-          setEditOpen(false);
         },
         onError() {
           toast.error("Something went wrong... please, try again!");
@@ -78,84 +89,63 @@ export default function PreferencesModal({
   };
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent className="p-0 bg-gray-50 overflow-hidden">
-        <DialogHeader className="p-4 border-b bg-white">
-          <DialogTitle>{value}</DialogTitle>
-        </DialogHeader>
-        <div className="px-4 pb-4 flex flex-col gap-y-2">
-          <Dialog open={editOpen} onOpenChange={setEditOpen}>
-            <DialogTrigger>
-              <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
-                <div className="flex items-center justify-between">
-                  <p className="text-sm font-semibold">Workspace Name</p>
-                  <p className="text-sm text-[#1264a3] hover:underline font-semibold">
-                    Edit
-                  </p>
+    <>
+      <ConfirmDialog />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+          <DialogHeader className="p-4 border-b bg-white">
+            <DialogTitle>{value}</DialogTitle>
+          </DialogHeader>
+          <div className="px-4 pb-4 flex flex-col gap-y-2">
+            <Dialog open={editOpen} onOpenChange={setEditOpen}>
+              <DialogTrigger>
+                <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm font-semibold">Workspace Name</p>
+                    <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                      Edit
+                    </p>
+                  </div>
+                  <p className="text-sm text-left">{value}</p>
                 </div>
-                <p className="text-sm text-left">{value}</p>
-              </div>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Rename this workspace</DialogTitle>
-              </DialogHeader>
-              <form action="" className="space-y-4" onSubmit={handleEdit}>
-                <Input
-                  value={value}
-                  disabled={updatingWorkspace}
-                  onChange={(e) => setValue(e.target.value)}
-                  required
-                  autoFocus
-                  minLength={3}
-                  maxLength={80}
-                  placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
-                />
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button variant="outline" disabled={updatingWorkspace}>
-                      Cancel
-                    </Button>
-                  </DialogClose>
-                  <Button disabled={updatingWorkspace}>Save</Button>
-                </DialogFooter>
-              </form>
-            </DialogContent>
-          </Dialog>
-          <Dialog open={removeOpen} onOpenChange={setRemoveOpen}>
-            <DialogTrigger asChild>
-              <button
-                className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
-                disabled={false}
-                onClick={() => {}}
-              >
-                <Trash className="size-4 " />
-                <p className="text-sm font-semibold">Delete workspace</p>
-              </button>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Remove this workspace</DialogTitle>
-                <p className="text-sm text-slate-600">
-                  Are you sure? This cannot be undone!
-                </p>
-              </DialogHeader>
-              <form onSubmit={handleRemove}>
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button variant="outline" disabled={removingWorkspace}>
-                      Cancel
-                    </Button>
-                  </DialogClose>
-                  <Button disabled={removingWorkspace} variant="destructive">
-                    Remove
-                  </Button>
-                </DialogFooter>
-              </form>
-            </DialogContent>
-          </Dialog>
-        </div>
-      </DialogContent>
-    </Dialog>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Rename this workspace</DialogTitle>
+                </DialogHeader>
+                <form action="" className="space-y-4" onSubmit={handleEdit}>
+                  <Input
+                    value={value}
+                    disabled={updatingWorkspace}
+                    onChange={(e) => setValue(e.target.value)}
+                    required
+                    autoFocus
+                    minLength={3}
+                    maxLength={80}
+                    placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+                  />
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline" disabled={updatingWorkspace}>
+                        Cancel
+                      </Button>
+                    </DialogClose>
+                    <Button disabled={updatingWorkspace}>Save</Button>
+                  </DialogFooter>
+                </form>
+              </DialogContent>
+            </Dialog>
+            <button
+              className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
+              disabled={false}
+              onClick={handleRemove}
+            >
+              <Trash className="size-4 " />
+              <p className="text-sm font-semibold">Delete workspace</p>
+            </button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
---
+++ src/hooks/use-confirm.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import { Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export const useConfirm = (
+  title: string,
+  message: string,
+): [() => JSX.Element, () => Promise<unknown>] => {
+  const [promise, setPromise] = useState<{
+    resolve: (value: boolean) => void;
+  } | null>(null);
+
+  const confirm = () =>
+    new Promise((resolve, reject) => {
+      setPromise({ resolve });
+    });
+
+  const handleClose = () => {
+    setPromise(null);
+  };
+
+  const handleCancel = () => {
+    promise?.resolve(false);
+    handleClose();
+  };
+
+  const handleConfirm = () => {
+    promise?.resolve(true);
+    handleClose();
+  };
+
+  const ConfirmDialog = () => {
+    return (
+      <Dialog open={promise !== null}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>{message}</DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="pt-2">
+            <Button variant="outline" onClick={handleCancel}>
+              Cancel
+            </Button>
+            <Button onClick={handleConfirm} variant="destructive">
+              Confirm
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  };
+
+  return [ConfirmDialog, confirm];
+};
